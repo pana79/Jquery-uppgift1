09b8fa7a7235a9f0cc4c2838f09b2643a12dd4eb
Patrik Näsström <patrik_nasstrom@hotmail.com>
2019-11-28 14:11:58 +0100

    login sidan

---
+++ script.js
@@ -0,0 +1,23 @@
+$(function){
+    function validateInput(id){
+        if()
+
+
+    }
+
+
+$('#firstName').keyup(function(){
+    validateInput('#fistName')
+    console.log('trigged')
+})
+
+$('#regForm').submit(e)=> {
+    e.preventDefault();
+
+    validateInput('#fistName')
+    validateInput('#lastName')
+
+
+}
+
+}
